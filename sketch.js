21f2fdbf193edc5800e08b325049335dd6b1cf3f
Coldaewon <deawon04072@gmail.com>
2024-11-26 14:12:39 +0900

    정리

--- sketch.js
+++ sketch.js
@@ -1,9 +1,31 @@
+let system;
+let systems = [];
+
+let g;
+let wind;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(720, 400);
+  g = createVector(0, 0.05);
+  wind = createVector(0.03, -0.01);
 }
 
 function draw() {
-  background(220);
+  background(51);
+
+  for(let s of systems){
+    s.addParticle();
+    s.applyGravity(g);
+    s.applyForce(wind);
+    s.run();
+  }
 }
 
+function mouseClicked(){
+  let mPos = createVector(mouseX, mouseY);
+  let system = new ParticleSystem(mPos);
+  system.push(system);
+}
+
+class ParticleSystem
 
